3bb3ecd19412e7ca57195a18ef1b6326b2fb8fb9
lvhb <lvhb@fitech.com.cn>
2016-04-25 14:47:09 +0800

    create new functions

---
+++ src/main/java/com/fitech/hadoop/phoenix/fun_currency_codeFunction.java
@@ -0,0 +1,53 @@
+package com.fitech.hadoop.phoenix;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.phoenix.expression.Expression;
+import org.apache.phoenix.expression.function.ScalarFunction;
+import org.apache.phoenix.parse.FunctionParseNode;
+import org.apache.phoenix.schema.tuple.Tuple;
+import org.apache.phoenix.schema.types.PDataType;
+import org.apache.phoenix.schema.types.PVarchar;
+
+@FunctionParseNode.BuiltInFunction(name = "CURRENCYCODE", args = {
+		@org.apache.phoenix.parse.FunctionParseNode.Argument(allowedTypes = { PVarchar.class }) })
+public class fun_currency_codeFunction extends ScalarFunction {
+	public static final String NAME="CURRENCYCODE";
+	private static final PDataType TYPE = PVarchar.INSTANCE;
+	public fun_currency_codeFunction(){}
+	public fun_currency_codeFunction(List<Expression> children) throws SQLException {
+		super(children);
+	}
+	@Override
+	public boolean evaluate(Tuple tuple, ImmutableBytesWritable ptr) {
+		if(!getCurrencyCodeExpression().evaluate(tuple, ptr)){
+			return false;
+		};
+		String currencyCode=(String) getCurrencyCodeExpression().getDataType().toObject(ptr, getCurrencyCodeExpression().getSortOrder());
+		String formu = "^[A-Za-z]{3}";
+	    Pattern p = Pattern.compile(formu);
+	    Matcher m = p.matcher(currencyCode);
+	    if (m.find()) {
+	        ptr.set(PVarchar.INSTANCE.toBytes("true"));
+	        return true;
+	    } else {
+	        ptr.set(PVarchar.INSTANCE.toBytes("false"));
+	        return true;
+	    }
+	}
+	private Expression getCurrencyCodeExpression() {
+		return children.get(0);
+	}
+	@Override
+	public PDataType getDataType() {
+		return TYPE;
+	}
+	@Override
+	public String getName() {
+		return NAME;
+	}
+}
